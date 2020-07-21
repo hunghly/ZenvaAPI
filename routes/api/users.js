bc677b03c14b569b5e8e26cbcfdbd52f3afe355c
Hung Ly <hunghly@gmail.com>
2020-07-20 20:38:45 -0500

    added error handling for user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,18 +5,29 @@ const router = Router();
 
 router.get('/', (req, res, next) => {
     User.find({}, (err, users) => {
+        if (err) return res.status(500).send({err});
         return res.send(users)
     })
 })
 
 router.post('/', (req, res, next) => {
     const {username} = req.body;
+    if (!username) {
+        return res.status(400).send({err: 'Required Fields are not found: username'});
+    }
     const newUser = new User({
         username: username
     })
     return newUser.save(function(err, model) {
-        return res.send(model);
+        if (err) {
+           return res.status(400).send({err})
+        }
+        return res.status(201).send(model);
     })
+})
+
+router.get('/err', (req, res, next) => {
+    return res.error;
 })
 
 
